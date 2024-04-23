202c7437ac69d44ab2fb0cabef48818c07a3837c
Zah-ar <33529958+Zah-ar@users.noreply.github.com>
2024-04-23 12:30:33 +0300

    1

--- GoodTemplate.php
+++ GoodTemplate.php
@@ -10,19 +10,31 @@ class GoodTemplate
     }
     private function setParams(&$result, $good)
     {
+        $sizeExist = true;
         if($good['size'] !=  'no defined' && mb_strtolower($good['size']) !=  'no difned' && mb_strtolower($good['size']) !=  'one size' && mb_strlen($good['size']) != 0)
         {
             $result = str_replace('%size%', strtoupper($good['size']), $result);
         }else{
             $result = str_replace('%size%', '', $result);
+            $result = str_replace("\n- Размер:", '', $result);
             $result = str_replace('- Размер:', '', $result);
             $result = str_replace('  ', '', $result);
+            $result = trim($result);
+            $sizeExist = false;
         }
         if($good['color'] !=  'no defined' && mb_strlen($good['color']) != 0)
         {
-            $result = str_replace('%color%', $good['color'], $result);
+            if(!$sizeExist)
+            {
+                $result = str_replace("\n- Цвет:", '- Цвет:', $result);
+                $result = str_replace('%color%', $good['color'], $result);
+                $result = trim($result);
+            }else{
+             $result = str_replace('%color%', $good['color'], $result);
+            }
         }else{
             $result = str_replace('%color%', '', $result);
+            $result = str_replace("\n- Цвет:", '', $result);      
             $result = str_replace('- Цвет:', '', $result);
             $result = str_replace('  ', '', $result);
         }
@@ -34,6 +46,7 @@ class GoodTemplate
         $result = $description;
         self::setUrl($result, $good);
         self::setParams($result, $good);
+
         return $result;
     }
 }
